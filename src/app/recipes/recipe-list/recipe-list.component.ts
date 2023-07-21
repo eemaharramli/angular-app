37bed75e583d7964d475602473b4aa0abf4096af
eemaharramli <elnurema@code.edu.az>
2023-07-21 23:46:13 +0400

    add ingredient.model, add ingredients array in ingredient-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
- recipes = [];
+ recipes: Recipe[] = [
+   new Recipe('A test Recipe', 'Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+   new Recipe('A test Recipe', 'Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+ ];
 }
